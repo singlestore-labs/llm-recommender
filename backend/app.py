e1b113a1ca699c8bd8029f24d177f438f955c4a0
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2023-12-18 15:33:25 -0800

    init database fn added

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,75 @@
 import os
 import requests
+import singlestoredb as s2
+import pandas as pd
 from dotenv import load_dotenv
 load_dotenv()
 
 HF_TOKEN = os.getenv('HF_TOKEN')
+DB_CONNECTION_URL = os.environ.get('DB_CONNECTION_URL')
+DB_NAME = os.environ.get('DB_NAME')
+
+db_connection = s2.connect(DB_CONNECTION_URL)
 
 
-def get_hf_model_downloads(repo_id: str):
+def load_leaderboard_df():
+    leaderboard_path = os.path.join('leaderboard/datasets/leaderboard.json')
+
+    if os.path.exists(leaderboard_path):
+        return pd.read_json(leaderboard_path, dtype={'still_on_hub': bool})
+    else:
+        print(f"The file '{leaderboard_path}' does not exists")
+
+
+def drop_table(table_name: str):
+    with db_connection.cursor() as cursor:
+        cursor.execute(f'DROP TABLE IF EXISTS {DB_NAME}.{table_name}')
+
+
+def create_models_table():
+    with db_connection.cursor() as cursor:
+        cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS models (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(512) NOT NULL,
+                author VARCHAR(512) NOT NULL,
+                repo_id VARCHAR(1024) NOT NULL,
+                score DECIMAL(5, 2) NOT NULL,
+                link VARCHAR(255) NOT NULL,
+                still_on_hub BOOLEAN NOT NULL,
+                downloads INT,
+                likes INT
+            )
+        ''')
+
+
+def fill_models_table():
+    with db_connection.cursor() as cursor:
+        cursor.execute('SELECT COUNT(*) FROM models')
+        has_records = bool(cursor.fetchall()[0][0])
+
+        if not has_records:
+            leaderboard_df = load_leaderboard_df()
+            values = leaderboard_df.to_records(index=True).tolist()
+
+            cursor.executemany(f'''
+                INSERT INTO models (id, name, author, repo_id, score, link, still_on_hub)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ''', values)
+
+
+def init_database():
+    create_models_table()
+    fill_models_table()
+
+
+def get_hf_model_details(repo_id: str):
     request_url = f'https://huggingface.co/api/models/{repo_id}'
     response = requests.get(request_url, headers={'Authorization': f'Bearer {HF_TOKEN}'})
 
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        return {'downloads': data['downloads'], 'likes': data['likes'], 'created_at': data['createdAt']}
     else:
         return None
 
@@ -24,7 +81,4 @@ def get_hf_model_readme(repo_id: str):
     if response.status_code == 200:
         return response.text
     else:
-        return ''
-
-
-# get_hf_model_downloads('rwitz2/go-bruins-v2.1.1')
+        return None
