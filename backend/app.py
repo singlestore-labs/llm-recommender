5982d50f98b4b92f848f7c03bec8031684556f4c
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2023-12-19 10:40:56 -0800

    readme, likes and downloads added to leaderboard

--- backend/app.py
+++ backend/app.py
@@ -12,51 +12,54 @@ DB_NAME = os.environ.get('DB_NAME')
 db_connection = s2.connect(DB_CONNECTION_URL)
 
 
-def load_leaderboard_df():
-    leaderboard_path = os.path.join('leaderboard/datasets/leaderboard.json')
+def init_database():
+    def load_leaderboard_df():
+        leaderboard_path = os.path.join('leaderboard/datasets/leaderboard.json')
 
-    if os.path.exists(leaderboard_path):
-        return pd.read_json(leaderboard_path, dtype={'still_on_hub': bool})
-    else:
-        print(f"The file '{leaderboard_path}' does not exists")
+        if os.path.exists(leaderboard_path):
+            return pd.read_json(leaderboard_path, dtype={'still_on_hub': bool})
+        else:
+            print(f"The file '{leaderboard_path}' does not exists")
+
+    def create_models_table():
+        with db_connection.cursor() as cursor:
+            cursor.execute(f'''
+                CREATE TABLE IF NOT EXISTS models (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(512) NOT NULL,
+                    author VARCHAR(512) NOT NULL,
+                    repo_id VARCHAR(1024) NOT NULL,
+                    score DECIMAL(5, 2) NOT NULL,
+                    link VARCHAR(255) NOT NULL,
+                    still_on_hub BOOLEAN NOT NULL,
+                    downloads INT,
+                    likes INT,
+                    readme TEXT
+                )
+            ''')
+
+    def fill_models_table():
+        with db_connection.cursor() as cursor:
+            cursor.execute('SELECT COUNT(*) FROM models')
+            has_records = bool(cursor.fetchall()[0][0])
+
+            if not has_records:
+                leaderboard_df = load_leaderboard_df()
+                values = leaderboard_df.to_records(index=True).tolist()
+
+                cursor.executemany(f'''
+                    INSERT INTO models (id, name, author, repo_id, score, link, still_on_hub)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ''', values)
+
+    # drop_table('models')
+    create_models_table()
+    fill_models_table()
 
 
 def drop_table(table_name: str):
     with db_connection.cursor() as cursor:
         cursor.execute(f'DROP TABLE IF EXISTS {DB_NAME}.{table_name}')
-
-
-def create_models_table():
-    with db_connection.cursor() as cursor:
-        cursor.execute(f'''
-            CREATE TABLE IF NOT EXISTS models (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(512) NOT NULL,
-                author VARCHAR(512) NOT NULL,
-                repo_id VARCHAR(1024) NOT NULL,
-                score DECIMAL(5, 2) NOT NULL,
-                link VARCHAR(255) NOT NULL,
-                still_on_hub BOOLEAN NOT NULL,
-                downloads INT,
-                likes INT,
-                readme TEXT
-            )
-        ''')
-
-
-def fill_models_table():
-    with db_connection.cursor() as cursor:
-        cursor.execute('SELECT COUNT(*) FROM models')
-        has_records = bool(cursor.fetchall()[0][0])
-
-        if not has_records:
-            leaderboard_df = load_leaderboard_df()
-            values = leaderboard_df.to_records(index=True).tolist()
-
-            cursor.executemany(f'''
-                INSERT INTO models (id, name, author, repo_id, score, link, still_on_hub)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-            ''', values)
 
 
 def get_models(select='*'):
@@ -70,39 +73,5 @@ def get_models_repo_id_df():
     return pd.DataFrame({'repo_id': repo_ids})
 
 
-def init_database():
-    # drop_table('models')
-    create_models_table()
-    fill_models_table()
-
-
-def get_hf_model_details(repo_id: str):
-    request_url = f'https://huggingface.co/api/models/{repo_id}'
-    response = requests.get(request_url, headers={'Authorization': f'Bearer {HF_TOKEN}'})
-
-    if response.status_code == 200:
-        data = response.json()
-        return {
-            'downloads': data.get('downloads', 0),
-            'likes': data.get('likes', 0),
-            'created_at': data.get('createdAt')
-        }
-    else:
-        return None
-
-
-def get_hf_model_readme(repo_id: str):
-    request_url = f'https://huggingface.co/{repo_id}/raw/main/README.md'
-    response = requests.get(request_url)
-
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
-
-
 def update_models():
     df = get_models_repo_id_df()
-
-    for i, row in df.iterrows():
-        details = get_hf_model_details(row['repo_id'])
--- backend/leaderboard/app.py
+++ backend/leaderboard/app.py
@@ -1,5 +1,7 @@
-import pandas as pd
+import os
+import requests
 from huggingface_hub import snapshot_download
+from dotenv import load_dotenv
 
 from src.display.utils import (
     BENCHMARK_COLS,
@@ -9,6 +11,10 @@ from src.display.utils import (
 from src.envs import EVAL_REQUESTS_PATH, EVAL_RESULTS_PATH, QUEUE_REPO, RESULTS_REPO, DATASETS_PATH
 from src.populate import get_leaderboard_df
 from src.tools.collections import update_collections
+
+load_dotenv()
+
+HF_TOKEN = os.getenv('HF_TOKEN')
 
 
 def download_results():
@@ -29,6 +35,35 @@ def download_results():
         print(e)
 
 
+def get_hf_model_details(repo_id: str):
+    request_url = f'https://huggingface.co/api/models/{repo_id}'
+    response = requests.get(request_url, headers={'Authorization': f'Bearer {HF_TOKEN}'})
+
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            'downloads': data.get('downloads', 0),
+            'likes': data.get('likes', 0),
+            'created_at': data.get('createdAt')
+        }
+    else:
+        return {
+            'downloads': 0,
+            'likes': 0,
+            'created_at': ''
+        }
+
+
+def get_hf_model_readme(repo_id: str):
+    request_url = f'https://huggingface.co/{repo_id}/raw/main/README.md'
+    response = requests.get(request_url)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return ''
+
+
 def create_dataset():
     raw_df = get_leaderboard_df(EVAL_RESULTS_PATH, EVAL_REQUESTS_PATH, COLS, BENCHMARK_COLS)
     update_collections(raw_df.copy())
@@ -38,6 +73,14 @@ def create_dataset():
         [["model", "author", "model_name_for_query", "average", "link", "still_on_hub"]]
         .rename(columns={"model": "name", "model_name_for_query": "repo_id", "average": "score"})
     )
+
+    for i, row in df.iterrows():
+        details = get_hf_model_details(row['repo_id'])
+        readme = get_hf_model_readme(row['repo_id'])
+        df.at[i, 'readme'] = readme
+        df.at[i, 'downloads'] = details['downloads']
+        df.at[i, 'likes'] = details['likes']
+        df.at[i, 'created_at'] = details['created_at']
 
     df.to_json(f'{DATASETS_PATH}/leaderboard.json', orient='records')
 
